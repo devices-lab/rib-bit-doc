330e153178873bbde6dd8b6e7eb0438630de5f66
Dr John Vidler <j.vidler@lancaster.ac.uk>
2026-02-23 18:09:40 +0000

    Updated configuration from the upstream package, moved to 3x column layout, updated to allow package to be embedded

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,28 @@
 import userDocBase from "@devices-lab/user-doc-base";
+import docToc from "@johnvidler/11ty-doc-toc";
 
 export default async function(eleventyConfig, options = {}) {
-    // Register user-doc-base plugin with this project's input dir
-    await userDocBase(eleventyConfig, { inputDir: "src", syncChanges: true });
+    const inputDir = options.inputDir || "src";
+    const outputDir = options.outputDir || "_site";
+    const layoutsDir = options.layoutsDir || "_layouts";
 
-    eleventyConfig.addPassthroughCopy("**/*.png");
-    eleventyConfig.addPassthroughCopy("**/*.jpg");
-    eleventyConfig.addPassthroughCopy("**/*.jpeg");
+    // Register user-doc-base plugin with this project's input dir
+    await userDocBase(eleventyConfig, { inputDir: inputDir, syncChanges: true });
+
+    eleventyConfig.addPassthroughCopy("src/**/*.png");
+    eleventyConfig.addPassthroughCopy("src/**/*.jpg");
+    eleventyConfig.addPassthroughCopy("src/**/*.jpeg");
+
+    const toc = await docToc(eleventyConfig, { root: inputDir });
+    await toc.addChapterPackage();
+
+    console.log( JSON.stringify(toc, null, 2) );
 
     return {
         dir: {
-            input: "src",
-            output: "_site",
-            layouts: "_layouts"  // Standard layouts dir (will be populated by plugin)
+            input: inputDir,
+            output: outputDir,
+            layouts: layoutsDir  // Standard layouts dir (will be populated by plugin)
         },
         templateFormats: ["md", "njk", "html", "css"],
         markdownTemplateEngine: "njk",
--- src/index.md
+++ src/index.md
@@ -3,6 +3,9 @@ layout: "devices-lab/default"
 title: "The Rib:bit Research Platform"
 image: "/img/tile.png"
 description: "The Rib:bit is a research platform extension for the Micro:bit"
+chapters:
+    - powering.md
+    - programming.md
 ---
 
 # Overview
